c8e94098e9c90a5f40096a2567b79b11bb9fabb2
JoGMG <joattah3@gmail.com>
2023-08-29 01:34:00 +0100

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+# Using Puppet, create a file in /tmp.
+# Requirements:
+# - File path is /tmp/school
+# - File permission is 0744
+# - File owner is www-data
+# - File group is www-data
+# - File contains I love Puppet
+
+file { 'create or update file with these configs':
+  ensure  => present,
+  path    => '/tmp/school'
+  mode    => '0744',
+  owner   => www-data,
+  group   => ww-data,
+  content => 'I love Puppet'
+}
