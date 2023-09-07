8be96a846f33d90d12897ad1d0b6e6c515ca21df
JoGMG <joattah3@gmail.com>
2023-09-07 02:09:49 +0100

    custom header with puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,34 @@
-# Installs a Nginx server with custom HTTP header
+# Install an Nginx server with custom HTTP header
 
-exec {'update':
-  provider => shell,
-  command  => 'sudo apt-get -y update',
-  before   => Exec['install Nginx'],
+package { 'nginx':
+  ensure => 'present',
 }
 
-exec {'install Nginx':
-  provider => shell,
-  command  => 'sudo apt-get -y install nginx',
-  before   => Exec['add_header'],
-}
+# exec {'update':
+#   provider => shell,
+#   command  => 'sudo apt-get -y update',
+#   before   => Exec['install Nginx'],
+# }
 
-exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+# exec {'install Nginx':
+#   provider => shell,
+#   command  => 'sudo apt-get -y install nginx',
+#   before   => Exec['add_header'],
+# }
+
+file_line { 'add custom header':
+  ensure      => 'present',
+  path        => '/etc/nginx/sites-available/default',
+  environment => ["var=${hostname}"],
+  after       => 'server {',
+  line        => '\n\tadd_header X-Served-By "$var";',
 }
+# exec { 'add_header':
+#   provider    => shell,
+#   environment => ["HOST=${hostname}"],
+#   command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+#   before      => Exec['restart Nginx'],
+# }
 
 exec { 'restart Nginx':
   provider => shell,
