9ff7f5e93387212235f4a29d104942e133171d77
JoGMG <joattah3@gmail.com>
2023-10-19 18:37:49 +0100

    increase user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,5 @@
 
 exec { 'Increase User Soft Limit':
     provider => shell,
-    command  => 'echo -e "holberton\t soft\t nofile\t 4000" >> /etc/security/limits.conf',
-    before   => Exec['Increase User Hard Limit'],
-}
-
-exec { 'Increase User Hard Limit':
-    provider => shell,
-    command  => 'echo -e "holberton\t hard\t nofile\t 5000" >> /etc/security/limits.conf',
+    command  => 'sed -Ei '/holberton\s*(hard|soft)/s/[0-9]+/5000/' /etc/security/limits.conf',
 }
