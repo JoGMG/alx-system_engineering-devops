bb7bfa366faa313fe58ee7be301930083cde5a86
JoGMG <joattah3@gmail.com>
2023-10-19 18:23:46 +0100

    checker

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,12 @@
 # Increase the file descriptor limit for a user.
 
-exec { 'Add to User-Specific Config file':
+exec { 'Increase User Soft Limit':
     provider => shell,
-    command  => 'echo -e "holberton\t hard\t nofile\t 65535" >> /etc/security/limits.conf',
+    command  => 'echo -e "holberton\t soft\t nofile\t 4000" >> /etc/security/limits.conf',
+    before   => Exec['Increase User Hard Limit'],
+}
+
+exec { 'Increase User Hard Limit':
+    provider => shell,
+    command  => 'echo -e "holberton\t hard\t nofile\t 5000" >> /etc/security/limits.conf',
 }
