b7f59a330ce6f373f9d3c4b5b9bb91e26fb6ab93
JoGMG <joattah3@gmail.com>
2023-10-19 18:18:31 +0100

    increase user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,6 @@
-# Increase the file descriptor limit for a user
+# Increase the file descriptor limit for a user.
 
-exec { 'Add to User-Specific Config file'
+exec { 'Add to User-Specific Config file':
     provider => shell,
-    command  => 'holberton soft nofile 5000',
-    command  => 'holberton hard nofile 6000',
+    command  => 'echo -e "holberton\t hard\t nofile\t 65535" >> /etc/security/limits.conf',
 }
