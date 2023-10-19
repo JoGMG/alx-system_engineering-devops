dd962eacb81911866ab10b2d763744d3a4c5e273
JoGMG <joattah3@gmail.com>
2023-10-19 18:52:17 +0100

    checker

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Increase the file descriptor limit for a user.
 
-exec { 'Increase User Soft Limit':
-    provider => shell,
-    command  => 'sudo sed -Ei '/holberton\s*(hard|soft)/s/(4|5)/5000/' /etc/security/limits.conf',
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile \(5\|4\)/nofile 5000/" /etc/security/limits.conf',
 }
